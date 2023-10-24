613725d8c09cef58c6b5cc09156372ee24a7e1a1
mtahirrc <146178769+mtahirrc@users.noreply.github.com>
2023-10-24 17:50:59 +0500

    quick fix

--- apply-config-to-sitemap.js
+++ apply-config-to-sitemap.js
@@ -14,12 +14,12 @@ const ApplyConfig = async (groupedUrls) => {
                 .getSpace(contentfulSpaceId)
                 .then((space) => space.getEnvironment("master"))
                 .then((environment) => environment.getContentType(ele.contentType.contentTypeId)).then(async (contentType) => {
-                    consoleInfo(`${new Date().toLocaleString()}: Content type with the ID '${contentType?.sys.id}' already exists.`);
-                    consoleInfo(`${new Date().toLocaleString()}: Creating new entry...`);
+                    consoleInfo(`Content type with the ID '${contentType?.sys.id}' already exists.`);
+                    consoleInfo(`Creating new entry...`);
                     await scrapeAllPages(urlsToScrap, ele.fields, ele.contentType, ele.domains, ele.slug); // Wait for the scraping to complete
                 }).catch(async (er) => {
-                    consoleInfo(`${new Date().toLocaleString()}: No content type with the ID '${ele.contentType?.contentTypeId}' found.`);
-                    consoleInfo(`${new Date().toLocaleString()}: Creating content type with ID '${ele.contentType?.contentTypeId}'...`);
+                    consoleInfo(`No content type with the ID '${ele.contentType?.contentTypeId}' found.`);
+                    consoleInfo(`Creating content type with ID '${ele.contentType?.contentTypeId}'...`);
                     await scrapeData(urlsToScrap[0], ele.fields, ele.domains).then(async (data) => {
                         await createContentType({ pageURL: urlsToScrap[0], data, passedcontentType: ele.contentType, passedfields: ele.fields }).then(async (res) => {
                             consoleSuccess(`Content type with ID '${ele.contentType?.contentTypeId} Created Successfully!`);
@@ -27,16 +27,6 @@ const ApplyConfig = async (groupedUrls) => {
                         });
                     })
                 })
-
-            // } catch (error) {
-            //     // Content type does not exist, create it
-            //     await scrapeData(urlsToScrap[0], ele.fields, ele.domains).then(async (data) => {
-            //         await createContentType({ pageURL: urlsToScrap[0], data, passedcontentType: ele.contentType, passedfields: ele.fields }).then(async (res) => {
-            //             consoleSuccess(`Content type with ID '${passedcontentType?.contentTypeId} Created Successfully!`);
-            //             await scrapeAllPages(urlsToScrap, ele.fields, ele.contentType, ele.domains, ele.slug); // Wait for the scraping to complete
-            //         });
-            //     })
-            // }
         } else {
             console.log("no pages defined for", ele.slug)
         }
